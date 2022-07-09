53d50993e00756c7ab57326ef83909e837dc4e50
Muhamamad Devano Zaidan <80044118+Devanoz@users.noreply.github.com>
2022-07-09 14:59:58 +0700

    nambah list kategori dengan card

--- src/views/lKategori/child/ListKategori.js
+++ src/views/lKategori/child/ListKategori.js
@@ -1,43 +1,47 @@
 import React from 'react'
 //improt data
 import {
-    CCardImage,
-    CCardTitle,
-    CCardText,
-    CAvatar,
-    CButton,
-    CButtonGroup,
-    CCard,
-    CCardBody,
-    CCardFooter,
-    CCardHeader,
-    CCol,
-    CProgress,
-    CRow,
-    CTable,
-    CTableBody,
-    CTableDataCell,
-    CTableHead,
-    CTableHeaderCell,
-    CTableRow,
-  }from '@coreui/react'
-  import dataKategori from '../data/dataKategori'
-import { data } from 'core-js/internals/is-forced';
+  CCardImage,
+  CCardTitle,
+  CCardText,
+  CAvatar,
+  CButton,
+  CButtonGroup,
+  CCard,
+  CCardBody,
+  CCardFooter,
+  CCardHeader,
+  CCol,
+  CProgress,
+  CRow,
+  CTable,
+  CTableBody,
+  CTableDataCell,
+  CTableHead,
+  CTableHeaderCell,
+  CTableRow,
+} from '@coreui/react'
+import dataKategori from '../data/dataKategori'
+import { data } from 'core-js/internals/is-forced'
 
-  export default function ListKategori() {
-    const titleKategori = dataKategori.map((data)=> data.title);
-    return(
-        <CCard style={{ width: '18rem' }}>
-        <CCardImage orientation="top" src="/images/react.jpg" />
-        <CCardBody>
-          <CCardTitle >
-
-          </CCardTitle>
-          <CCardText>
-            ...
-          </CCardText>
-          <CButton href="#">...</CButton>
-        </CCardBody>
-      </CCard>
-    )
-  }
+export default function ListKategori() {
+  const titleKategori = dataKategori.map((data) => data.title)
+  return (
+    <CRow>
+      {titleKategori.map((value) => {
+        return (
+          <CCol md={4}>
+            <CCard style={{ width: '18rem' }}>
+              <CCardImage orientation="top" src="../../../assets/images/react.jpg" />
+              <CCardBody>
+                <CCardTitle></CCardTitle>
+                <CCardText>...</CCardText>
+                <CButton href="#">lihat</CButton>
+              </CCardBody>
+            </CCard>
+          </CCol>
+        )
+      })}
+    </CRow>
+  )
+}
