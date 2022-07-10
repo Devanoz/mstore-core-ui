fb1dce4badc3ca2cfcac4d7fbe370605688f6623
Muhamamad Devano Zaidan <80044118+Devanoz@users.noreply.github.com>
2022-07-10 21:59:26 +0700

    semnagat bro

--- src/views/lProduk/data/dataProduk.js
+++ src/views/lProduk/data/dataProduk.js
@@ -1,8 +1,8 @@
 const produk = [
   {
     id:1,
-    tittle: 'kaos malioboro',
-    price: 150000,
+    tittle: 'kaos kece',
+    price: 190000,
     stock: 350,
     image: 'none',
     weight: 1,
@@ -14,37 +14,104 @@ const produk = [
   },
   {
     id:2,
-    tittle: 'kaos malioboro',
-    price: 150000,
+    tittle: 'Batik Kece',
+    price: 900000,
     stock: 350,
     image: 'none',
     weight: 1,
     catogory: {
-      tittle: 'kaos',
+      tittle: 'Batik',
       image: 'none',
     }
   },
   {
     id:3,
-    tittle: 'kaos malioboro',
+    tittle: 'Tas Kece',
     price: 150000,
     stock: 350,
     image: 'none',
     weight: 1,
     catogory: {
-      tittle: 'kaos',
+      tittle: 'Tas',
       image: 'none',
     }
   },
   {
     id:4,
-    tittle: 'kaos malioboro',
+    tittle: 'Gantungan Kunci Abadi',
     price: 150000,
     stock: 350,
     image: 'none',
     weight: 1,
     catogory: {
-      tittle: 'kaos',
+      tittle: 'gantunganKunci',
+      image: 'none',
+    }
+  },
+  {
+    id:5,
+    tittle: 'Sandal khas jogja',
+    price: 1500000,
+    stock: 350,
+    image: 'none',
+    weight: 1,
+    catogory: {
+      tittle: 'Sandal',
+      image: 'none',
+    }
+  },{
+    id:4,
+    tittle: 'Blangkon Anti Air',
+    price: 898988989,
+    stock: 350,
+    image: 'none',
+    weight: 1,
+    catogory: {
+      tittle: 'Blangkon',
+      image: 'none',
+    }
+  },{
+    id:6,
+    tittle: 'Kaos Anti Air',
+    price: 9090909,
+    stock: 350,
+    image: 'none',
+    weight: 1,
+    catogory: {
+      tittle: 'Kaos',
+      image: 'none',
+    }
+  },{
+    id:7,
+    tittle: 'Sandal Merah Merona',
+    price: 70000,
+    stock: 350,
+    image: 'none',
+    weight: 1,
+    catogory: {
+      tittle: 'Sandal',
+      image: 'none',
+    }
+  },{
+    id:8,
+    tittle: 'Batik Bhineka',
+    price: 454545454,
+    stock: 350,
+    image: 'none',
+    weight: 1,
+    catogory: {
+      tittle: 'Batik',
+      image: 'none',
+    }
+  },{
+    id:9,
+    tittle: 'Batik polos',
+    price: 13456678,
+    stock: 350,
+    image: 'none',
+    weight: 1,
+    catogory: {
+      tittle: 'Batik',
       image: 'none',
     }
   },
